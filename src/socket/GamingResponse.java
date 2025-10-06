7b0adbdf5b036dc93506e58f44293f372669b51c
jocao@clarkson.edu <jocao@clarkson.edu>
2025-10-06 15:26:20 -0700

    corrected subclass

--- src/socket/GamingResponse.java
+++ src/socket/GamingResponse.java
@@ -1,15 +1,17 @@
 package socket;
 
-public class GamingResponse {
+public class GamingResponse extends Response{
     private int move;
     private boolean active;
 
     public GamingResponse(){
+        super();
         this.move = move;
         this.active = active;
     }
 
     public GamingResponse(int move, boolean active){
+        super();
         this.move = move;
         this.active = active;
     }
@@ -18,7 +20,7 @@ public class GamingResponse {
 
     public boolean getActive(){ return active; }
 
-    public void setMove(int movew){ this.move = move; }
+    public void setMove(int move){ this.move = move; }
 
     public void setActive(boolean active){ this.active = active;}
 
