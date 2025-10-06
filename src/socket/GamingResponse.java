6eb34e00f36be1598614bf20cefd0b68f8dc5a7b
jocao@clarkson.edu <jocao@clarkson.edu>
2025-10-06 14:33:19 -0700

    added gamingresponse

--- src/socket/GamingResponse.java
+++ src/socket/GamingResponse.java
@@ -1,4 +1,26 @@
 package socket;
 
 public class GamingResponse {
+    private int move;
+    private boolean active;
+
+    public GamingResponse(){
+        this.move = move;
+        this.active = active;
+    }
+
+    public GamingResponse(int move, boolean active){
+        this.move = move;
+        this.active = active;
+    }
+
+    public int getMove(){ return move; }
+
+    public boolean getActive(){ return active; }
+
+    public void setMove(int movew){ this.move = move; }
+
+    public void setActive(boolean active){ this.active = active;}
+
+
 }
